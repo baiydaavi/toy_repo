16bb5606319b75a0d58b51db3332618d0f59b132
Avinash <baidyaavinash@gmail.com>
2020-04-08 20:47:07 -0700

    "Just added the the Three_state_CA to complete my three state CA script for the homework"

--- three_state.py
+++ three_state.py
@@ -48,3 +48,76 @@ def lookup_table(rule_number):
     return dict(zip(neighborhoods, map(int,reversed(rule_ternary)))) # use map so that outputs are ints, not strings
 
 
+class Three_state_CA(object):
+    '''
+    Three state cellular automata simulator.
+    '''
+    
+    #__init__ is known as the constructor which simply allows you to define the attributes of the instantiated object from a given class that you want to pass as paramters
+    # Here we want that Three_state_CA class have attributes rule number and an initial condition to be passed as parameters as that is all you need to know for the evolution of the cellular automata
+    
+    def __init__(self, rule_number, initial_condition):
+        '''
+        Initializes the simulator for the given rule number and initial condition.
+        
+        Parameters: These are as attributes too. We just desire for these attributes to be passed as parameters.
+        ----------
+        rule_number: int
+            Integer value between 0 and 3**9-1, inclusive. The total number of possible rules are 3**9 for
+            the three state CA case.
+            
+        initial_condition: list
+            String used as the initial condition for the three state CA. Elements of the list should be 0s,1s 
+            and 2s. 
+        
+        Attributes
+        ----------
+        lookup_table: dict
+            Lookup table for the three state CA given as a dictionary, with neighborhood tuple keys. 
+        initial: array_like
+            Copy of the initial conditions used to instantiate the simulator
+        spacetime: array_like
+            2D array (list of lists) of the spacetime field created by the simulator.
+        current_configuration: array_like
+            List of the spatial configuration of the three state CA at the current time
+        '''
+        # we will see a cleaner and more efficient way to do the following when we introduce numpy
+        for i in initial_condition:
+            if i not in [0,1,2]:
+                raise ValueError("initial condition must be a list of 0s, 1s and 2s")
+                
+        self.lookup_table = lookup_table(rule_number)
+        self.initial = initial_condition
+        self.spacetime = [initial_condition]
+        self.current_configuration = initial_condition.copy()
+        self._length = len(initial_condition) #the underscore before _length defines a private variable
+
+    def evolve(self, time_steps):  #functions within a class is known as methods. Evolve is a method of this class
+        '''
+        Evolves the current configuration of the three state CA for the given number of time steps.
+        
+        Parameters
+        ----------
+        time_steps: int
+            Positive integer specifying the number of time steps for evolving the ECA.  
+        '''
+        if time_steps < 0:
+            raise ValueError("time_steps must be a non-negative integer")
+            
+        # try converting time_steps to int and raise a custom error if this can't be done
+        try:
+            time_steps = int(time_steps)
+        except ValueError:
+            raise ValueError("time_steps must be a non-negative integer")
+
+        for _ in range(time_steps): # use underscore if the index will not be used
+            new_configuration = []
+            for i in range(self._length):
+
+                neighborhood = (self.current_configuration[(i-1)], 
+                                self.current_configuration[i])
+
+                new_configuration.append(self.lookup_table[neighborhood])
+
+            self.current_configuration = new_configuration
+            self.spacetime.append(new_configuration)
